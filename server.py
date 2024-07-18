197df3de40c23f4f3c2a3551e618529cda324d24
LOVISH CHUGH <lchugh@uwaterloo.ca>
2024-07-18 17:58:28 -0400

    -Albert's changes
    Co-authored-by: aknduros <aknduros@users.noreply.github.com>

---
+++ server.py
@@ -0,0 +1,13 @@
+import numpy as p 
+
+
+# Create new parking lot data structure 
+class parkign_lot:
+    current_lot_image = None
+    parking_space_dictionary =
+
+ WAait for image from serer 
+
+# pass the image to the parking lot detection alg and return the updated Parking Lot Data structure   
+
+#prepare data for webserver 
