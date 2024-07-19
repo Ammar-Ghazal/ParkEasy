f23cd0136af55e04807127634edcff9f28351c9c
shahryarahmed <ahmedshahryar333@gmail.com>
2024-07-19 12:41:23 -0400

    landing page + admin page created

--- webapp/webapp/src/App.js
+++ webapp/webapp/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import AdminPage from './components/AdminPage';
 
-function App() {
+export default function App() {
+  console.log("App component is rendering");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<LandingPage />} />
+    //     <Route path="/admin" element={<AdminPage />} />
+    //   </Routes>
+    // </Router>
+    <LandingPage />
   );
 }
-
-export default App;
