82d6ab073d892948eacd0aa2e451ce45c1489b0f
Ammar-Ghazal <a4ghazal@uwaterloo.ca>
2024-07-19 14:23:35 -0400

    First attempt at cleaning up the graphics, still needs work and debugging

--- parkinglotlayout/attempt6.py
+++ parkinglotlayout/attempt6.py
@@ -15,6 +15,71 @@ def draw_rectangle(graphic, x, y, width, height, color):
     height = int(height/2)
     cv2.rectangle(graphic, (x-width, y-height), (x, y+height), color, thickness=2)
 
+def cleanup(result):
+    # Clean up results:
+    margin = 0.4
+    num_parking_lots = len(result["predictions"])
+    first_item = True
+    working_set = []
+    for i in range(num_parking_lots-1):
+        curr_prediction = result["predictions"][i]
+        x_curr = int(curr_prediction["x"])
+        y_curr = int(curr_prediction["y"])
+        width_curr = int(curr_prediction["width"])
+        height_curr = int(curr_prediction["height"])
+
+        next_prediction = result["predictions"][i+1]
+        x_next = int(next_prediction["x"])
+        y_next = int(next_prediction["y"])
+        width_next = int(next_prediction["width"])
+        height_next = int(next_prediction["height"])
+        
+        if (first_item):
+            working_set.append(i)
+            first_item = False
+        else:
+            if ((1-margin) <= y_curr/y_next <= (1+margin)):
+                # The y values are close enough, now check the x values:
+                if ((1-margin) <= ((x_curr+width_curr)/x_next) <= (1+margin)):
+                    # If the x values are also close, add current prediction:
+                    working_set.append(i)
+                    print("Appended to working set!")
+                else:
+                    format(result, working_set)
+                    working_set.clear()
+                    first_item = True
+            else:
+                working_set.clear()
+                first_item = True
+
+def format(result, working_set):
+    length = len(working_set)
+    average_width = 0
+    average_height = 0
+
+    for i in range(length):
+        index = working_set[i]
+
+        average_width += result["predictions"][index]["width"]
+        average_height += result["predictions"][index]["height"]
+
+    average_width = int(average_width/length)
+    average_height = int(average_height/length)
+
+    # Extract x and y values of first box:
+    first_x = result["predictions"][working_set[0]]["x"]
+    first_y = result["predictions"][working_set[0]]["y"]
+
+
+    # Now reassign width, height, x and y values
+    for i in range(length):
+        index = working_set[i]
+        result["predictions"][index]["width"] = average_width
+        result["predictions"][index]["height"] = average_height
+
+        result["predictions"][index]["x"] = int(first_x + i*average_width)
+        result["predictions"][index]["y"] = int(first_y + i*average_height)
+
 def scan_parking_lot(image_path):
     CLIENT = InferenceHTTPClient(
         api_url="https://detect.roboflow.com",
@@ -55,8 +120,10 @@ def scan_parking_lot(image_path):
     # Create a blank image
     parking_lot_graphic = np.zeros((image_height, image_width, 3), dtype=np.uint8)
 
+    cleanup(result)
+
     # Draw bounding boxes on the blank image
-    for prediction in result['predictions']:
+    for prediction in result["predictions"]:
         x = int(prediction["x"])
         y = int(prediction["y"])
         width = int(prediction["width"])
